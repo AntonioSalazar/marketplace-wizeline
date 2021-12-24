cd1cff52ea17c57999c5b09e653efb32e8c509da
Antonio Salazar <antonio.salazar@wizeline.com>
2021-12-23 18:29:12 -0600

    adding tailwind config adding the layout of the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', '/components/**/*.{js,ts,jsx,tsx}'],
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        "8xl": "1920px"
+      },
+    },
   },
   plugins: [],
 }
