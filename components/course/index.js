dde6c4547017433f074e7fe0535afc3ad5f099f7
Antonio Salazar <antonio.salazar@wizeline.com>
2021-12-25 23:41:43 -0600

    design is complet

--- components/course/index.js
+++ components/course/index.js
@@ -1,1 +1,4 @@
 export { default as List } from './list'
+export { default as Curriculum } from './curriculum'
+export { default as Keypoints } from './keypoints'
+export { default as Hero } from './hero'
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,18 @@
-/* eslint-disable @next/next/no-img-element */
-import { Footer, Navbar, BreadCrumbs, Hero } from '@components/common';
+import { BreadCrumbs, Hero } from '@components/common';
 import { EthRates, WalletBar} from '@components/web3'
 import { Card } from '@components/order'
 import { List } from '@components/course';
+import { Layout } from '@components/layout';
 
 export default function Home() {
   return (
-    <div>
-      <div className="relative bg-white overflow-hidden">
-        <div className="relative max-w-7xl mx-auto px-4">
-          <Navbar />
-          <div className="fit">
-            <Hero />
-            <BreadCrumbs />
-            <WalletBar />
-            <EthRates />
-            <Card />
-            <List />
-          </div>
-        </div>
-        <Footer />
-      </div>
-    </div>
+    <Layout>
+      <Hero />
+      <BreadCrumbs />
+      <WalletBar />
+      <EthRates />
+      <Card />
+      <List />
+    </Layout>
   )
 }
